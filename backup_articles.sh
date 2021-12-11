c62509eabe42ed66ad33afa35f6518c0f7f5336e
gezm0 <eremin@ryazan.net.ru>
2021-12-11 13:07:20 +0300

    One for backup whole database, another for backup only result of query from database and sql file with query for it (bak for older version). More checkups.

--- backup_articles.sh
+++ backup_articles.sh
@@ -7,15 +7,18 @@ path_back=/local/backups
 db_user=study
 db_host=db
 db_name=final_task
+db_query=/local/scripts/backup_articles.sql
 
 # making query and compressing result
-psql -h $db_host -U $db_user -d $db_name -f /local/scripts/backup_articles.sql | gzip -9 > $path_dump/$db_name.`date +%F`.articles.sql.gz
+psql -h $db_host -U $db_user -d $db_name -f $db_query | gzip -9 > $path_dump/$db_name.`date +%F`.articles.sql.gz
 
-# moving dumps older than 3 days to backups storage
-find $path_dump/ -name "*.articles.sql.gz" -type f -mtime +3 -exec mv '{}' $path_back \;
+# check for condition 'no more than 3 files' and moving dumps to backups storage if there more then 3
+
+find $path_dump -name "*.articles.sql.gz" -type f -printf '%T@ %i\n' | sort -n | head -n -3 | while read tstamp inode; \
+do find $path_dump -inum "$inode" -exec mv '{}' $path_back \;; done
 
 # deleting REALLY old backups
-find $path_back/ -name "*.articles.sql.gz" -type f -mtime +180 -delete
+find $path_back -name "*.articles.sql.gz" -type f -mtime +90 -delete
 
 # clearing variable
 unset PGPASSWORD
--- backup_db.sh
+++ backup_db.sh
@@ -11,11 +11,13 @@ db_name=final_task
 # dumping database and compressing it
 pg_dump -h $db_host -U $db_user -d $db_name | gzip -9 > $path_dump/$db_name.`date +%F`.all.sql.gz
 
-# moving dumps older than 3 days to backups storage
-find $path_dump/ -name "*.all.sql.gz" -type f -mtime +3 -exec mv '{}' $path_back \;
+# check for condition 'no more than 3 files' and moving dumps to backups storage if there more then 3
+
+find $path_dump -name "*.all.sql.gz" -type f -printf '%T@ %i\n' | sort -n | head -n -3 | while read tstamp inode; do \
+find $path_dump -inum "$inode" -exec mv '{}' $path_back \;; done
 
 # deleting REALLY old backups
-find $path_back/ -name "*.all.sql.gz" -type f -mtime +180 -delete
+find $path_back -name "*.all.sql.gz" -type f -mtime +90 -delete
 
 # clearing variable
 unset PGPASSWORD
