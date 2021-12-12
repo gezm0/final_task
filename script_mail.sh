4e68866ba34ae3862448e57cedfd24eda6a9be35
gezm0 <eremin@ryazan.net.ru>
2021-12-12 18:50:23 +0300

    second bash script for sending mail with even more checkups and more configuration (more comments added)

--- script_mail.sh
+++ script_mail.sh
@@ -1,41 +1,53 @@
 #!/bin/sh
 
-pid=/var/run/script_mail.pid
-files=/local/files
-rcpt="root@localhost"
-sender="nightwatch@localhost"
-subj=$(echo "Report on ${files}")
-body=/tmp/script_mail.tmp
-max_files=3
-max_size=1000
+# config start
+pid=/var/run/script_mail.pid		# name for pid file
+files=/local/backups			# name of directory for monitoring
+rcpt="root@localhost"			# recipient of message
+sender="nightwatch@localhost"		# sender of message
+subj=$(echo "Report on ${files}")	# subject string for message
+body=/tmp/script_mail.tmp		# temporary body of message
+max_files=3				# maximum number of files for trigger
+max_size=1000				# maximum size of files for trigger (in bytes)
+# config end
 
+# make pid file
 echo $$ > ${pid}
 
+# count current number of files and size of those files
 cur_num=$(ls -l ${files} | grep "^-" | wc -l)
 cur_size=$(du -b ${files} | cut -f1)
 
+# check if message temporary body already exists
 if [ -f ${body} ]
 then
+# silently remove it if exists
 	rm -f ${body}
 fi
 
+# check condition for number of files and append information to message body if fits
 if [ ${cur_num} -gt ${max_files} ] 
 then
 	echo "There are already" ${cur_num} "files in" ${files} >> ${body}
 fi
 
+# check condition for size of files and append information to message body if fits
 if [ ${cur_size} -gt ${max_size} ]
 then
 	echo "There are already" ${cur_size} "bytes in" ${files} >> ${body}
 fi
 
+# check if message temporary body exists
 if [ -f ${body} ]
 then
+# if exists then add timestamp and send it out
 	echo "---" >> ${body}
 	echo "Generated at" `date` >> ${body}
 	mail -s "${subj}" -r ${sender} ${rcpt} < ${body}
 else
+# if not exist then report to console that nothing to do
 	echo "Nothing to do"
 fi
 
+# silently remove temporary body of message
 rm -f ${body}
